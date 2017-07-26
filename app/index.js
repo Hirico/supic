cfdb6a91fddc0cb0001c84360422c3dd36d37b12
151250216 <151250216@smail.nju.edu.cn>
2017-07-26 16:38:03 +0800

    fix<app>: reorganize structure

--- app/index.js
+++ app/index.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import Root from './containers/Root';
-import { configureStore, history } from './store/configureStore';
+import ReactDOM from 'react-dom';
+import Root from './containers/DemoPage';
 import './app.global.css';
 
-const store = configureStore();
-
-render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
+ReactDOM.render(
+  <Root/>,
   document.getElementById('root')
 );
 
 if (module.hot) {
-  module.hot.accept('./containers/Root', () => {
-    const NextRoot = require('./containers/Root'); // eslint-disable-line global-require
-    render(
-      <AppContainer>
-        <NextRoot store={store} history={history} />
-      </AppContainer>,
+  module.hot.accept('./containers/DemoPage', () => {
+    const NextRoot = require('./containers/DemoPage'); // eslint-disable-line global-require
+    ReactDOM.render(
+      <NextRoot />,
       document.getElementById('root')
     );
   });
